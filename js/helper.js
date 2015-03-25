5f1179fd628c5a480a73ce58223d3906a5ef02ea
rp-1 <richpaschall@comcast.net>
2015-03-25 11:08:39 -0500

    General tweaks to main objects

--- js/helper.js
+++ js/helper.js
@@ -26,8 +26,13 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+//var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+//var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+/* Modified original skill variables to make it so list items went within ul opening and closing tags */
+var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3>';
+var HTMLskillList = '<ul id="skills">%data%</ul>';
+var HTMLskills = '<li><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,9 +8,61 @@ var bio = {
         "gitHub": "http://www.github.com/rp-1",
         "location": "Nashville, TN"
     },
-    "welcome_msg": "Hi, I'm areative director, writer, strategist, developer and overall brand missionary who touches all stages of the marketing process to engineer creative solutions that help businesses solve problems and reach goals.",    
-    "skills": ["Creative Direction", "Concept Development", "Web development", "Programming", "Copywriting"],
-    "pic_url": "http://placehold.it/200x200"
+    "welcomeMessage": "Hi, I'm areative director, writer, strategist, developer and overall " +
+    "brand missionary who touches all stages of the marketing process to engineer creative " +
+    "solutions that help businesses solve problems and reach goals.",    
+    "skills": [
+        "Creative Direction",
+        "Concept Development",
+        "Web development",
+        "Programming",
+        "Copywriting"
+    ],
+    
+    "biopic": "images/biopic.jpg",
+    "display": function() {
+        var formattedName = HTMLheaderName.replace("%data%", this.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", this.role);
+
+        var formattedMobile = HTMLmobile.replace("%data%", this.contacts.mobile);
+        var formattedEmail = HTMLemail.replace("%data%", this.contacts.email);
+        var formattedTwitter = HTMLtwitter.replace("%data%", this.contacts.twitter);
+        var formattedGitHub = HTMLgithub.replace("%data%", this.contacts.gitHub);
+
+        var formattedLoc = HTMLlocation.replace("%data%", this.contacts.location);
+
+        var formattedBiopic = HTMLbioPic.replace("%data%", this.biopic);
+        var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
+
+        /* skills */
+        var skills_string = this.skills.join(" ");
+        var skills = HTMLskills.replace("%data%", skills_string);
+
+        $("#header").prepend(formattedRole);
+        $("#header").prepend(formattedName);
+        
+        var formattedContacts = 
+            formattedMobile +
+            formattedEmail +
+            formattedTwitter +
+            formattedGitHub +
+            formattedLoc;
+        $('#topContacts').append(formattedContacts);
+        $('#footerContacts').append(formattedContacts);
+        
+
+        $("#header").append(formattedBiopic);
+        $("#header").append(formattedWelcomeMessage);
+
+        if(this.skills.length !== 0) {
+            $("#header").append(HTMLskillsStart);
+            
+            for(var i = 0; i < this.skills.length; i++) {
+                var formattedSkill = HTMLskills.replace("%data%", this.skills[i]);
+                $("#header").append(formattedSkill);
+            }
+        }
+    }
         
 };
 
@@ -19,36 +71,40 @@ var work = {
         {
             "employer": "Asheton Freelance",
             "title": "President",
+            "location": "Nashville, TN",
             "dates": "2014 - present",
-            "loc": "Nashville, TN",
-            "description": "Freelance integrated creative director available for concept and development projects requiring big ideas and a smart, integrated approach."
+            "description": "Freelance integrated creative director available for concept and " +                    "development projects requiring big ideas and a smart, integrated approach."
         },
 
         {
             "employer": "Beehive",
             "title": "Executive Creative Director",
+            "location": "Nashville, TN",
             "dates": "2013 - 2014",
-            "loc": "Nashville, TN",
-            "description": "Led creative department for mid-size, full-service advertising agency. Played a critical role in increasing agency revenue, generating insights and ideas that helped score multiple new business wins, two of which were major account acquisitions for the agency.In less than 6 months with the organization was promoted to Senior Vice President, Director of Creative Strategy."
+            "description": "Led creative department for mid-size, full-service advertising agency. " +              "Played a critical role in increasing agency revenue, generating insights and ideas " +                 "that  helped score multiple new business wins, two of which were major account " +                     "acquisitions for the agency.In less than 6 months with the organization was " + 
+            "promoted to Senior Vice President, Director of Creative Strategy."
         },
         
         {
             "employer": "LOVED",
             "title": "Executive Creative Director",
+            "location": "Birmingham, AL",
             "dates": "2001 - 2013",
-            "loc": "Birmingham, AL",
-            "description": "Established creative vision for all clients. Brought focus and consistency to brand messaging across all platforms. Brought both strategic and creative vision to large-scale, multi-million dollar projects with many moving parts.Managed diverse array of creative partners and vendors. Handled countless product launches and major communications initiatives for regional and international consumer goods accounts. Created custom branded content and digital assets, everything from apps to ebooks to rich media banners to full-blown webisode/reality series."
+            "description": "Established creative vision for all clients. Brought focus and " +
+            "consistency to brand messaging across all platforms. Brought both strategic and " +                    "creative vision to large-scale, multi-million dollar projects with many moving " +                     "parts.Managed diverse array of creative partners and vendors.Handled countless " +                     "product launches and major communications initiatives for regional and " +
+            "international consumer goods accounts. Created custom branded content and digital " +
+            "assets, everything from apps to ebooks to rich media banners to full-blown " +                         "webisode/reality series."
         }
     
     ],
     
-    "displayWork": function() {
+    "display": function() {
     
         for(var i = 0; i < this.jobs.length; i++) {
             var formattedEmployer = HTMLworkEmployer.replace("%data%", this.jobs[i].employer);
             var formattedTitle = HTMLworkTitle.replace("%data%", this.jobs[i].title);
             var formattedDates = HTMLworkDates.replace("%data%", this.jobs[i].dates);
-            var formattedLoc = HTMLworkLocation.replace("%data%", this.jobs[i].loc);
+            var formattedLoc = HTMLworkLocation.replace("%data%", this.jobs[i].location);
             var formattedDescription = HTMLworkDescription.replace("%data%", this.jobs[i].description);
 
             $("#workExperience").append(HTMLworkStart);
@@ -65,7 +121,7 @@ var projects = {
         {
             "title": "Bullboard App",
             "dates": "2011-2012",
-            "description": "Programmed (Objective-C) and designed meme-style app. Thousands of downloads in App Store",
+            "description": "Programmed (Objective-C) and designed meme-style app. Thousands of downloads in App Store.",
             "images": [
                 {
                     "thumb": "http://placehold.it/200x100",
@@ -81,7 +137,9 @@ var projects = {
         {
             "title": "Stockscape",
             "dates": "2012",
-            "description": "Created engaging stock app that visualized stocks as hot-air balloons, rising and falling with their stock price, in real-time.",
+            "description": "Created engaging stock app that visualized stocks as hot-air " +
+            "balloons, rising and falling with their stock price, in real-time. Currently " +
+            "available for iPad in the App Store.",
             "images": [
                 {
                     "thumb": "http://placehold.it/200x100",
@@ -95,7 +153,7 @@ var projects = {
         }
     ],
     
-    "displayProjects": function() {
+    "display": function() {
     
         for(var i = 0; i < this.projects.length; i++) {
             var formattedTitle = HTMLprojectTitle.replace("%data%", this.projects[i].title);
@@ -123,18 +181,20 @@ var education = {
     "schools": [
         {
             "name": "University of Georgia",
+            "location": "Athens, Gerogia",
             "degree": "B.A. English",
+            "majors": ["English"],
             "dates": "1991-1995",
-            "loc": "Athens, Gerogia",
-            "major": "English"
+            "url": "http://uga.edu"
         },
 
         {
             "name": "Portfolio Center",
+            "location": "Atlanta, Gerogia",
             "degree": "Advertising Arts",
+            "majors": ["Advertising"],
             "dates": "1996-1997",
-            "loc": "Atlanta, Gerogia",
-            "major": "Advertising"
+            "url": "http://portfoliocenter.com"
         }
     ],
 
@@ -142,104 +202,61 @@ var education = {
         {
             "title": "Front End Developer Nano Degree Program",
             "school": "Udacity",
-            "dates": "2015",
+            "date": "2015",
             "url": "http://www.udacity.com/"
         },
 
         {
             "title": "Managerial Accounting",
             "school": "University of Washington",
-            "dates": "2006",
+            "date": "2006",
             "url": "http://www.uwa.edu/"
         }
     ],
 
-    "displayEducation": function() {
+    "display": function() {
 
         /* -- TRADITIONAL SCHOOLS -- */
         for(var i = 0; i < education.schools.length; i++) {
-            var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
-            var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
-            var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
-            var formattedLoc = HTMLschoolLocation.replace("%data%", education.schools[i].loc);
-            var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+            var formattedName = HTMLschoolName.replace("%data%", this.schools[i].name);
+            var formattedDegree = HTMLschoolDegree.replace("%data%", this.schools[i].degree);
+            var formattedDates = HTMLschoolDates.replace("%data%", this.schools[i].dates);
+            var formattedLoc = HTMLschoolLocation.replace("%data%", this.schools[i].location);
+            var formattedMajor = HTMLschoolMajor.replace("%data%", this.schools[i].majors);
 
             $("#education").append(HTMLschoolStart);
-            $(".education-entry:last").append(formattedName);
-            $(".education-entry:last").append(formattedDegree);
+            $(".education-entry:last").append(formattedName + formattedDegree);
             $(".education-entry:last").append(formattedDates);
             $(".education-entry:last").append(formattedLoc);
             $(".education-entry:last").append(formattedMajor);
         }
 
-
-        if(education.onlineCourses.length > 0) {
             $("#education").append(HTMLonlineClasses);
-        }
 
         /* -- ONLINE CLASSES -- */
         for(var i = 0; i < education.onlineCourses.length; i++) {
-            var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
-            var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
-            var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
-            var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+            var formattedTitle = HTMLonlineTitle.replace("%data%", this.onlineCourses[i].title);
+            var formattedSchool = HTMLonlineSchool.replace("%data%", this.onlineCourses[i].school);
+            var formattedDates = HTMLonlineDates.replace("%data%", this.onlineCourses[i].date);
+            var formattedUrl = HTMLonlineURL.replace("%data%", this.onlineCourses[i].url);
 
-            $("#education").append(HTMLonlineClassesStart);
-            $(".education-entry:last").append(formattedTitle);
-            $(".education-entry:last").append(formattedSchool);
+            $("#education").append(HTMLschoolStart);
+            $(".education-entry:last").append(formattedTitle + formattedSchool);
+            console.log(formattedTitle + formattedSchool);
             $(".education-entry:last").append(formattedDates);
             $(".education-entry:last").append(formattedUrl);
         }
     }
 };
 
-    
-
-/* -- MAIN INFO -- */
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-var mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var email = HTMLemail.replace("%data%", bio.contacts.email);
-var twitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var gitHub = HTMLgithub.replace("%data%", bio.contacts.gitHub);
-
-var loc = HTMLlocation.replace("%data%", bio.contacts.location);
-
-var pic = HTMLbioPic.replace("%data%", bio.pic_url);
-var welcome = HTMLWelcomeMsg.replace("%data%", bio.welcome_msg);
-
-/* skills */
-var skills_string = bio.skills.join(" ");
-var skills = HTMLskills.replace("%data%", skills_string);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-$("#topContacts").append(mobile);
-$("#topContacts").append(email);
-$("#topContacts").append(twitter);
-$("#topContacts").append(gitHub);
-$("#topContacts").append(loc);
-
-$("#header").append(pic);
-$("#header").append(welcome);
-
-if(bio.skills.length !== 0) {
-    $("#header").append(HTMLskillsStart);
-    for(var i = 0; i < bio.skills.length; i++) {
-        var new_skill = HTMLskills.replace("%data%", bio.skills[i]);
-        $("#header").append(new_skill);
-    }
-}
-
-work.displayWork();
-
-projects.displayProjects();
-
-education.displayEducation();
-
-    
+/*
+Add formatted html to page
+*/
+bio.display();
+work.display();
+projects.display();
+education.display();
+   
 $("#main").append(internationalizeButton);
 
 function inName(name) {
